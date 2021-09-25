5eeb3875b275cc7531f1eee2965449e031ba6071
1902667-JacksonWong <1902667@sit.singaporetech.edu.sg>
2021-09-26 00:42:40 +0800

    beacons information such as mac,name,rssi from mokoApp to flask
    Done with Jeremy

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,20 +1,34 @@
 from logging import Handler
 from flask import Flask, request
 from flask.json import jsonify
+import json
 
 app = Flask(__name__)
 
+# global beacon_list 
+beacon_list=[]
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    global beacon_list
     if request.method == "GET":
-        print("helloGet")
-    if request.method == "POST":
-        print("helloPost")
-        data = request.data
-        print(data)
-    return "<h1>hello world</h1>"
+        # print("helloGet")
+        return str(beacon_list)
 
+    if request.method == "POST":
+        # print("helloPost")
+        data = request.data
+        # print(data)
+        #Extracting JSON
+        print("--------")
+        beacons = json.loads((data).decode("UTF-8"))
+        if isinstance(beacons, dict):
+            temp = str(beacons['beacons'])
+            beacon_list = beacons['beacons']
+            print(temp)
+            return f"<h1>{temp}</h1>"
+        
+    
 def handle_request():
     return "Successful Connection"
 
